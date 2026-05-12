b780a34d755ae19198a3d790cadc48ce0f84bda7
vineethkarri <148723927+vineethkarri@users.noreply.github.com>
2026-05-12 14:39:20 -0400

    Remove docstring from rush function
    Removed docstring from the rush function.

--- rush-1-1/rush.py
+++ rush-1-1/rush.py
@@ -1,15 +1,5 @@
 import sys
-
-
 def rush(x, y):
-    """
-    Display a square pattern based on x (width) and y (height).
-
-    Corners use 'o'.
-    Horizontal edges (top/bottom) use '-'.
-    Vertical edges (left/right) use '|'.
-    Interior cells are spaces.
-    """
     if x <= 0 or y <= 0:
         print("Invalid size", file=sys.stderr)
         return
